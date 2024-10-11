5d2ae271beb1758c15c80378e7bfdd2dbb2a626d
joaovitorvas <joaovitorvasc.2307@outlook.com>
2024-10-11 08:27:33 -0300

    filtrando impressoras com pouca tinta

--- Controle De Impressoras/Controllers/HomeController.cs
+++ Controle De Impressoras/Controllers/HomeController.cs
@@ -9,9 +9,20 @@ namespace Controle_De_Impressoras.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index(string tipo, string marca, string modelo, int patrimonio = 0)
+        public ActionResult Index(string tipo, string marca, string modelo, int patrimonio = 0, bool? tintaBaixa = false)
         {
             var impressoras = PrintersModel.RecuperarImpressoras(tipo, marca, modelo, patrimonio);
+
+            if (tintaBaixa.HasValue && tintaBaixa.Value)
+            {
+
+                impressoras = impressoras.Where(i =>
+                    i.PorcentagemBlack < 10 ||
+                    (i.PorcentagemCyan < 10 && i.Tipo == "COLOR") ||
+                    (i.PorcentagemMagenta < 10 && i.Tipo == "COLOR") ||
+                    (i.PorcentagemYellow < 10 && i.Tipo == "COLOR")).ToList();
+            }
+
             return View(impressoras);
         }
 
@@ -23,14 +34,12 @@ namespace Controle_De_Impressoras.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
