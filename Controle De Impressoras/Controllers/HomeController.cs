baa3cc05ede4e7f6269423403a8e3ffc85c83dbd
joaovitorvas <joaovitorvasc.2307@outlook.com>
2024-11-11 13:47:17 -0300

    Filtros funcionando

--- Controle De Impressoras/Controllers/HomeController.cs
+++ Controle De Impressoras/Controllers/HomeController.cs
@@ -43,20 +43,31 @@ namespace Controle_De_Impressoras.Controllers
             return View();
         }
 
-        public ActionResult Painel(string tipo, string marca, string modelo, int patrimonio = 0, bool? tintaBaixa = false)
+        public ActionResult Painel(string tipo, string marca, string modelo, int patrimonio = 0, bool tintaBaixa = false, bool intervalo1 = false, bool intervalo2 = false, bool intervalo3 = false, bool esgotado = false)
         {
             var impressoras = PrintersModel.RecuperarImpressoras(tipo, marca, modelo, patrimonio);
 
-            if (tintaBaixa.HasValue && tintaBaixa.Value)
+            if (tintaBaixa)
             {
-                impressoras = impressoras.Where(i =>
-                    i.PorcentagemBlack < 10 ||
-                    (i.PorcentagemCyan < 10 && i.Tipo == "COLOR") ||
-                    (i.PorcentagemMagenta < 10 && i.Tipo == "COLOR") ||
-                    (i.PorcentagemYellow < 10 && i.Tipo == "COLOR")).ToList();
+                impressoras = impressoras.Where(i => i.PorcentagemBlack < 10).ToList();
             }
+
+            if (!intervalo1 && !intervalo2 && !intervalo3 && !esgotado)
+            {
+                return View(impressoras);
+            }
+
+            impressoras = impressoras.Where(i =>
+                (intervalo1 && i.PorcentagemBlack <= 100 && i.PorcentagemBlack > 60) ||
+                (intervalo2 && i.PorcentagemBlack <= 60 && i.PorcentagemBlack > 20) ||
+                (intervalo3 && i.PorcentagemBlack <= 20 && i.PorcentagemBlack > 1) ||
+                (esgotado && i.PorcentagemBlack == 0)
+            ).ToList();
 
             return View(impressoras);
         }
+
+
+
     }
 }
