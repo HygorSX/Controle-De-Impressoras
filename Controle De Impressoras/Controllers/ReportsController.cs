8995ec7d1338b01f129b0c81f19347fa5753d9f3
Hygor <hygorlimatop123@gmail.com>
2024-12-02 09:55:21 -0300

    correção no commit anterior

--- Controle De Impressoras/Controllers/ReportsController.cs
+++ Controle De Impressoras/Controllers/ReportsController.cs
@@ -40,43 +40,5 @@ namespace Controle_De_Impressoras.Controllers
 
             return View(reports);
         }
-
-        public ActionResult PrinterDetails(int printerId)
-        {
-            var firstDayOfMonth = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
-            var lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddDays(-1);
-
-            Console.WriteLine($"First Day: {firstDayOfMonth}, Last Day: {lastDayOfMonth}");
-
-            var printerDetails = _context.PrinterStatusLogs
-                .Where(p => p.PrinterId == printerId && p.DataHoraDeBusca >= firstDayOfMonth && p.DataHoraDeBusca <= lastDayOfMonth)
-                .OrderBy(p => p.DataHoraDeBusca)
-                .ToList();
-
-            Console.WriteLine($"Número de registros encontrados: {printerDetails.Count}");
-
-            if (printerDetails.Any())
-            {
-                var firstRecord = printerDetails.First();
-                var lastRecord = printerDetails.Last();
-
-                Console.WriteLine($"Primeiro Registro: {firstRecord.QuantidadeImpressaoTotal}, Último Registro: {lastRecord.QuantidadeImpressaoTotal}");
-
-                int? totalPrints = lastRecord.QuantidadeImpressaoTotal - firstRecord.QuantidadeImpressaoTotal;
-
-                Console.WriteLine($"Total de Impressões: {totalPrints}");
-
-                ViewBag.ImpressaoTotal = totalPrints; 
-            }
-            else
-            {
-                ViewBag.ImpressaoTotal = 0; 
-                Console.WriteLine("Nenhum registro encontrado para este mês.");
-            }
-
-            ViewBag.PrinterDetails = _context.Printers.Find(printerId);
-
-            return View();
-        }
     }
 }
